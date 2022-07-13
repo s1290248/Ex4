01bd9a0c67aaff630778bba5dfcb021f0276302c
s1290248 <108911274+s1290248@users.noreply.github.com>
2022-07-13 11:05:15 +0900

    Won/lost message modified

--- ex4.c
+++ ex4.c
@@ -32,9 +32,9 @@ int main(){
   printf("Heads: %d, Tails: %d\n", heads, tails);
   
   if( heads > tails ){
-    printf("You won!\n");
+    printf("%s won!\n", name);
   }
-  else printf("You lost!\n");
+  else printf("%s lost!\n", name);
   
   return 0;
 }
